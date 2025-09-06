324d44dff73ab9f3aee74d9802315dc06c75a5bd
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-06 21:38:06 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -2,3 +2,21 @@
 Given an integer numRows, return the first numRows of Pascal's triangle.
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result(numRows);
+
+        for(int i=0; i<numRows; i++){
+            result[i] = vector<int>(i+1, 1);
+
+            for(int j=1; j<i; j++){
+                result[i][j] = result[i-1][j] + result[i-1][j-1];
+            }
+        }
+
+        return result;
+    }
+};
