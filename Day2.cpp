01c880f75533c6b97f37a9e94a767b65dd4578e0
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-07 21:48:07 +0530

    Update Day2.cpp

--- Day2.cpp
+++ Day2.cpp
@@ -1,4 +1,4 @@
-2561. Rearranging Fruits:
+/* 2561. Rearranging Fruits:
 You have two fruit baskets containing n fruits each. You are given two 0-indexed integer arrays basket1 and basket2 representing the cost of fruit in each basket. 
 You want to make both baskets equal. To do so, you can use the following operation as many times as you want:
 
@@ -6,4 +6,4 @@ Choose two indices i and j, and swap the ith fruit of basket1 with the jth fruit
 The cost of the swap is min(basket1[i], basket2[j]).
 Two baskets are considered equal if sorting them according to the fruit cost makes them exactly the same baskets.
 
-Return the minimum cost to make both the baskets equal or -1 if impossible.
+Return the minimum cost to make both the baskets equal or -1 if impossible. */
