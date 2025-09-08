d251af5385291faca18c77659d6cc7aca1d805f3
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-08 21:51:46 +0530

    Update Day3.cpp

--- Day3.cpp
+++ Day3.cpp
@@ -7,3 +7,104 @@ to the left or right. It takes one step to move one unit on the x-axis, and you 
 you harvest all the fruits at that position, and the fruits will disappear from that position.
 
 Return the maximum total number of fruits you can harvest. */
+
+
+/* class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int n = fruits.size();
+        vector<int> prefixSum(n);
+        vector<int> indices(n);   // positions(n)
+
+        // Build prefix sum and extract indices
+        for (int i = 0; i < n; i++) {
+            indices[i]   = fruits[i][0];
+            prefixSum[i] = fruits[i][1] + (i > 0 ? prefixSum[i - 1] : 0);
+        }
+
+        int maxFrutis = 0;
+
+        for (int d = 0; d <= k / 2; d++) {
+            // Move
+            int remain = k - 2 * d;
+            int i  = startPos - d;
+            int j  = startPos + remain;
+
+            int left = lower_bound(indices.begin(), indices.end(), i) - indices.begin();
+            int right = upper_bound(indices.begin(), indices.end(), j) - indices.begin() - 1;
+
+            if(left <= right) {
+                int total = prefixSum[right] - (left > 0 ? prefixSum[left - 1] : 0);
+                maxFrutis = max(maxFrutis, total);
+            }
+
+            // Second case: move right x, then left (k - 2x)
+            i  = startPos - remain;
+            j  = startPos + d;
+            
+            left  = lower_bound(indices.begin(), indices.end(), i) - indices.begin();
+            right = upper_bound(indices.begin(), indices.end(), j) - indices.begin() - 1;
+
+            if(left <= right) {
+                int total = prefixSum[right] - (left > 0 ? prefixSum[left - 1] : 0);
+                maxFrutis = max(maxFrutis, total);
+            }
+        }
+
+        return maxFrutis;
+    }
+}; */
+
+
+
+
+
+
+// Method 2
+class Solution {
+    public:
+        int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k){
+            int n = fruits.size();
+
+            vector<int> prefixSum(n);   // fruits prefixSum
+            vector<int> positions(n);   // sorted already 
+
+            for(int i=0; i<n; i++){
+                positions[i] = fruits[i][0];
+                prefixSum[i] = fruits[i][1] + (i > 0 ? prefixSum[i-1] : 0);
+            }
+
+            int maxFruits = 0;
+
+            for(int d=0; d <= k/2; d++){
+                // case 1 - moved 'd' steps to the left 
+                int remain = k-2*d;
+                int i = startPos - d;
+                int j = startPos + remain;
+
+                int left = lower_bound(begin(positions), end(positions), i) - begin(positions);
+                int right = upper_bound(begin(positions), end(positions), j) - begin(positions) - 1;
+
+
+                if(left <= right){
+                    int total = prefixSum[right] - (left > 0 ? prefixSum[left-1] : 0);
+                    maxFruits = max(maxFruits, total);
+                }
+
+                // case 2 - moved 'd' to the right hand side
+                // remain = k - 2*d;
+                i = startPos - remain;
+                j = startPos + d;
+
+                left = lower_bound(begin(positions), end(positions), i) - begin(positions);
+                right = upper_bound(begin(positions), end(positions), j) - begin(positions) - 1;
+
+                if(left <= right) {
+                    int total = prefixSum[right] - (left > 0 ? prefixSum[left-1] : 0);
+                    maxFruits = max(maxFruits, total);
+                }
+            }
+
+            return maxFruits;
+        }
+};
