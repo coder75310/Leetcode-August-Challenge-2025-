54904e8a9a7907dd5b5ee1efca55a17eb2d0516d
Ankit Singh <2023pcecsankit019@poornima.org>
2025-09-10 19:58:12 +0530

    Update day4.cpp

--- day4.cpp
+++ day4.cpp
@@ -11,3 +11,38 @@ Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
 Given the integer array fruits, return the maximum number of fruits you can pick. */
 
 
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+
+        unordered_map<int, int> mp;
+
+        int i = 0;
+        int j = 0;
+        int count = 0;
+
+        while(j < n) {
+            mp[fruits[j]]++;
+
+            if(mp.size() <= 2) {
+                count = max(count, j-i+1);
+            } else {
+                mp[fruits[i]]--;
+                if(mp[fruits[i]] == 0)
+                    mp.erase(fruits[i]);
+                i++;
+            }
+
+            j++;
+        }
+        return count;
+    }
+};
+
+
+
+
+
+
+
